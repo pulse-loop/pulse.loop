0914b725e8a8b35bed64fc689b7a9fbcdf5668e8
Riccardo Persello <riccardo.persello+i@icloud.com>
2022-08-19 18:06:19 +0200

    Initial Commit

---
+++ pulse.loop/pulse_loopApp.swift
@@ -0,0 +1,17 @@
+//
+//  pulse_loopApp.swift
+//  pulse.loop
+//
+//  Created by Riccardo Persello on 19/08/22.
+//
+
+import SwiftUI
+
+@main
+struct pulse_loopApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
