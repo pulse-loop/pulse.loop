629e9e064db0e57c4d5411fb19db0072b57dd9e7
Riccardo Persello <riccardo.persello@icloud.com>
2022-12-23 19:09:18 +0100

    Removed fastlane.

--- fastlane/Appfile.swift
+++
@@ -1,9 +0,0 @@
-var appIdentifier: String { return "com.persello.pulse-loop" } // The bundle identifier of your app
-var appleID: String { return "riccardo.persello@icloud.com" } // Your Apple Developer Portal username
-
-var itcTeam: String? { return "122337905" } // App Store Connect Team ID
-var teamID: String { return "BZTVD28H6K" } // Apple Developer Portal Team ID
-
-
-// For more information about the Appfile, see:
-//     https://docs.fastlane.tools/advanced/#appfile
