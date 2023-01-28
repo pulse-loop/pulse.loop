462ed9d1d97161099b5906c744320f90cc37a4e3
Riccardo Persello <riccardo.persello@icloud.com>
2023-01-28 18:41:04 +0100

    Moved chart stack to another file.

--- pulse.loop/Views/TimingWindowConfigurationView.swift
+++ pulse.loop/Views/TimingWindowConfigurationView.swift
@@ -8,29 +8,6 @@
 import SwiftUI
 import Charts
 
-struct ChartStackView<Data: RawSensorDataProtocol>: View {
-    @ObservedObject var rawSensorData: Data
-    
-    var body: some View {
-        HStack {
-            ChartView(value: rawSensorData.ambient, title: "Ambient", lineColor: .blue, smooth: false)
-                .frame(width: 300, height: 320)
-            
-            ChartView(value: rawSensorData.led1MinusAmbient, title: "LED1 - Ambient", lineColor: .yellow, smooth: false)
-                .frame(width: 300, height: 320)
-            
-            ChartView(value: rawSensorData.led1, title: "LED1", lineColor: .green, smooth: false)
-                .frame(width: 300, height: 320)
-            
-            ChartView(value: rawSensorData.led2, title: "LED2", lineColor: .red, smooth: false)
-                .frame(width: 300, height: 320)
-            
-            ChartView(value: rawSensorData.led3, title: "LED3", lineColor: .gray, smooth: false)
-                .frame(width: 300, height: 320)
-        }
-    }
-}
-
 struct TimingWindowConfigurationView<Device>: View where Device: DeviceProtocol {
     @ObservedObject var device: Device
     
