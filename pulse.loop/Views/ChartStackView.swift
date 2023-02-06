579ee77c38e669721fa17086fb6d68f992179d28
Riccardo Persello <riccardo.persello@icloud.com>
2023-02-06 14:35:22 +0100

    One chart per channel.

--- pulse.loop/Views/ChartStackView.swift
+++ pulse.loop/Views/ChartStackView.swift
@@ -16,27 +16,42 @@ struct ChartStackView<SensorData: RawSensorDataProtocol>: View {
     
     var body: some View {
         HStack {
-            ChartView(value: rawSensorData.aggregatedData)
-//            OldChartView(value: rawSensorData.aggregatedData, title: "Ambient", lineColor: .blue, smooth: false)
-                .frame(width: 300, height: 320)
-//
-//            OldChartView(value: rawSensorData.led1MinusAmbient, title: "LED1 - Ambient", lineColor: .yellow, smooth: false)
-//                .frame(width: 300, height: 320)
-//
-//            OldChartView(value: rawSensorData.led1, title: "LED1", lineColor: .green, smooth: false)
-//                .frame(width: 300, height: 320)
-//
-//            OldChartView(value: rawSensorData.led2, title: "LED2", lineColor: .red, smooth: false)
-//                .frame(width: 300, height: 320)
-//
-//            OldChartView(value: rawSensorData.led3, title: "LED3", lineColor: .gray, smooth: false)
-//                .frame(width: 300, height: 320)
+            ChartView(
+                value: rawSensorData.aggregatedData,
+                dataSets: [.ambient],
+                title: "Ambient"
+            )
+            .frame(width: 300, height: 320)
+            
+            ChartView(
+                value: rawSensorData.aggregatedData,
+                dataSets: [.led1],
+                title: "LED 1"
+            )
+            .frame(width: 300, height: 320)
+            
+            ChartView(
+                value: rawSensorData.aggregatedData,
+                dataSets: [.led2],
+                title: "LED 2"
+            )
+            .frame(width: 300, height: 320)
+            
+            ChartView(
+                value: rawSensorData.aggregatedData,
+                dataSets: [.led3],
+                title: "LED 3"
+            )
+            .frame(width: 300, height: 320)
         }
     }
 }
 
 struct ChartStackView_Previews: PreviewProvider {
     static var previews: some View {
-        ChartStackView(rawSensorData: FakeDevice().rawSensorData)
+        let device = FakeDevice()
+        device.connect()
+        
+        return ChartStackView(rawSensorData: device.rawSensorData)
     }
 }
